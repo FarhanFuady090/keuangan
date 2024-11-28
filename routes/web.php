231a99d0b1a3285da6b51aa2ef7b864a2bb7f88a
FarhanFuady090 <ngabellserra@gmail.com>
2024-11-29 00:35:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+Route::get('/manage-user', action: function () {
+    return view(view: 'manage-user');
+});
+Route::get('/manage-unit-pendidikan', function () {
+    return view(view: 'manage-unit-pendidikan');
+});
+Route::get('/manage-jenis-pembayaran', function () {
+    return view(view: 'manage-jenis-pembayaran');
+});
+Route::get('/manage-data-siswa', action: function () {
+    return view(view: 'manage-data-siswa');
+});
+Route::get('/manage-kelas', function () {
+    return view('manage-kelas');
+});
+Route::get('/manage-tahun-ajaran', action: function () {
+    return view(view: 'manage-tahun-ajaran');
+});
+Route::get('/manage-biaya', action: function () {
+    return view(view: 'manage-biaya');
 });
