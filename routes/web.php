892b5ed4c0a6ee5c054ee302d72a71d065625caa
FarhanFuady090 <muhammadfarhanfuady@gmail.com>
2024-12-03 15:00:14 +0700

    update seputar login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,31 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-use App\Http\Controllers\Auth\LoginController;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/', [LoginController::class, 'login']);
-// Route::middleware(['auth', 'role:admin'])->group(function () {
-//     Route::get('/admin/manage-biaya', function () {
-//         return view('admin.manage-biaya');
-//     })->name('admin.manage-biaya');
-// });
-Route::get('/admin/manage-data-siswa', function () {
-    return view('admin.manage-data-siswa'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::get('/admin/manage-jenis-pembayaran', function () {
-    return view('admin.manage-jenis-pembayaran'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::get('/admin/manage-kelas', function () {
-    return view('admin.manage-kelas'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::get('/admin/manage-tahun-ajaran', function () {
-    return view('admin.manage-tahun-ajaran'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::get('/admin/manage-unit-pendidikan', function () {
-    return view('admin.manage-unit-pendidikan'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::get('/admin/manage-user', function () {
-    return view('admin.manage-user'); // Ganti dengan view yang sesuai
-})->middleware('auth');
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
